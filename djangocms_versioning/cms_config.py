f483f32d20717b9b363f76e3bfd71de7f79070e3
Anirban Lahiri <anirban.lahiri@fil.com>
2018-07-10 15:25:45 +0100

    minor tidying up 2

--- djangocms_versioning/cms_config.py
+++ djangocms_versioning/cms_config.py
@@ -22,11 +22,10 @@ class VersioningCMSExtension(CMSAppExtension):
         if not isinstance(cms_config.versioning_models, collections.abc.Iterable):
             raise ImproperlyConfigured(
                 "versioning_models not defined as a list")
-        elif cms_config.versioning_models:
-            self._version_models.extend(cms_config.versioning_models)
+
+        self._version_models.extend(cms_config.versioning_models)
 
     def configure_app(self, cms_config):
-        # stub function created to help integrate with Kzrysztof code later
         self.handle_versioning_models_setting(cms_config)
         content_model_n = [model._meta.get_field('content').rel.model for model in cms_config.versioning_models]
         replace_admin_for_models(content_model_n)

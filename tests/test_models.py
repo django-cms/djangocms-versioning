93266b3aa776fec36038a6b929f99cb36fa2e227
Monika Sulik <kontakt@monikasulik.net>
2018-08-08 14:03:53 +0100

    WIP: run custom copy functions in copy method

--- tests/test_models.py
+++ tests/test_models.py
@@ -1,3 +1,5 @@
+from mock import Mock, patch
+
 from freezegun import freeze_time
 
 from django.db.models import Q
@@ -6,8 +8,10 @@ from django.utils.timezone import now
 from cms.test_utils.testcases import CMSTestCase
 
 from djangocms_versioning.constants import DRAFT
+from djangocms_versioning.datastructures import VersionableItem
 from djangocms_versioning.models import Version
 from djangocms_versioning.test_utils import factories
+from djangocms_versioning.test_utils.relationships import models
 
 
 class CopyTestCase(CMSTestCase):
@@ -46,3 +50,24 @@ class CopyTestCase(CMSTestCase):
             original_version.content.poll,
             new_version.content.poll,
         )
+
+    def test_defining_copy_method_for_1to1_works(self):
+        grouper = models.Grouper1to1F.objects.create()
+        old_rel = models.OneToOneF.objects.create()
+        old_content = models.Content1to1F.objects.create(grouper=grouper, rel=old_rel)
+        old_version = Version.objects.create(content=old_content)
+        new_rel = models.OneToOneF.objects.create()
+        versionable = VersionableItem(
+            content_model=models.Content1to1F,
+            grouper_field_name='grouper',
+            copy_functions={
+                'rel': lambda old_content: new_rel
+            }
+        )
+        versionables_mock = Mock(cms_extension=Mock(
+            versionables_by_content={models.Content1to1F: versionable}))
+
+        with patch('djangocms_versioning.models.apps.get_app_config', Mock(return_value=versionables_mock)):
+            new_version = old_version.copy()
+
+        self.assertEqual(new_version.content.rel.pk, new_rel.pk)

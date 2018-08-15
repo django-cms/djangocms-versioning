884fd957f7b34593db935c054d016ea3b4e1fe06
Monika Sulik <kontakt@monikasulik.net>
2018-08-15 16:12:13 +0100

    Don't display publish on toolbar if not draft

--- tests/test_toolbars.py
+++ tests/test_toolbars.py
@@ -3,6 +3,7 @@ from django.test import RequestFactory
 from cms.test_utils.testcases import CMSTestCase
 from cms.toolbar.toolbar import CMSToolbar
 
+from djangocms_versioning import constants
 from djangocms_versioning.cms_toolbars import VersioningToolbar
 from djangocms_versioning.test_utils.factories import (
     UserFactory, PollVersionFactory)
@@ -10,33 +11,61 @@ from djangocms_versioning.test_utils.polls.cms_config import PollsCMSConfig
 
 
 class VersioningToolbarTestCase(CMSTestCase):
-    # TODO: Only show publish link for drafts
 
-    def setUp(self):
-        self.version = PollVersionFactory()
-
-    def _get_toolbar(self):
+    def _get_toolbar(self, version):
+        """Helper method to set up the toolbar
+        """
         request = RequestFactory().get('/')
         request.user = UserFactory()
         request.session = {}
         cms_toolbar = CMSToolbar(request)
         toolbar = VersioningToolbar(
             request, toolbar=cms_toolbar, is_current_app=True, app_path='/')
-        toolbar.toolbar.obj = self.version.content
+        toolbar.toolbar.obj = version.content
         return toolbar
 
-    def _get_admin_url(self):
+    def _get_publish_url(self, version):
+        """Helper method to return the expected publish url
+        """
         versionable = PollsCMSConfig.versioning[0]
         admin_url = self.get_admin_url(
-            versionable.version_model_proxy, 'publish', self.version.pk)
+            versionable.version_model_proxy, 'publish', version.pk)
         return admin_url
 
-    def test_publish_button_added(self):
-        toolbar = self._get_toolbar()
+    def test_publish_in_toolbar_for_draft_version(self):
+        version = PollVersionFactory(state=constants.DRAFT)
+        toolbar = self._get_toolbar(version)
 
         toolbar.post_template_populate()
 
         publish_button = toolbar.toolbar.get_right_items()[0].buttons[0]
         self.assertEqual(publish_button.name, 'Publish')
-        self.assertEqual(publish_button.url, self._get_admin_url())
+        self.assertEqual(publish_button.url, self._get_publish_url(version))
         self.assertFalse(publish_button.disabled)
+
+    def test_publish_not_in_toolbar_for_archived_version(self):
+        version = PollVersionFactory(state=constants.ARCHIVED)
+        toolbar = self._get_toolbar(version)
+
+        toolbar.post_template_populate()
+
+        self.assertListEqual(
+            toolbar.toolbar.get_right_items()[0].buttons, [])
+
+    def test_publish_not_in_toolbar_for_published_version(self):
+        version = PollVersionFactory(state=constants.PUBLISHED)
+        toolbar = self._get_toolbar(version)
+
+        toolbar.post_template_populate()
+
+        self.assertListEqual(
+            toolbar.toolbar.get_right_items()[0].buttons, [])
+
+    def test_publish_not_in_toolbar_for_unpublished_version(self):
+        version = PollVersionFactory(state=constants.UNPUBLISHED)
+        toolbar = self._get_toolbar(version)
+
+        toolbar.post_template_populate()
+
+        self.assertListEqual(
+            toolbar.toolbar.get_right_items()[0].buttons, [])

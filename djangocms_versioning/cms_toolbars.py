76dd825c81ed2ce5b6b899a478d4144598c2704d
Krzysztof Socha <ksocha@ksocha.com>
2018-08-20 18:08:17 +0200

    Edit redirect endpoint (#42)

--- djangocms_versioning/cms_toolbars.py
+++ djangocms_versioning/cms_toolbars.py
@@ -1,13 +1,11 @@
 from django.apps import apps
-from django.contrib.contenttypes.models import ContentType
-from django.core.urlresolvers import reverse
+from django.urls import reverse
 from django.utils.translation import ugettext_lazy as _
 
 from cms.toolbar.items import ButtonList
 from cms.toolbar_base import CMSToolbar
 from cms.toolbar_pool import toolbar_pool
 
-from djangocms_versioning.constants import DRAFT
 from djangocms_versioning.models import Version
 
 
@@ -29,7 +27,8 @@ class VersioningToolbar(CMSToolbar):
         """
         versioning_extension = apps.get_app_config(
             'djangocms_versioning').cms_extension
-        return versioning_extension.is_content_model_versioned(self.toolbar.obj.__class__)
+        return versioning_extension.is_content_model_versioned(
+            self.toolbar.obj.__class__)
 
     def _get_proxy_model(self):
         """Helper method to get the proxy model class for the content
@@ -37,35 +36,55 @@ class VersioningToolbar(CMSToolbar):
         """
         return self._get_versionable().version_model_proxy
 
-    def _add_publish_button(self, item):
+    def _add_publish_button(self):
         """Helper method to add a publish button to the toolbar
         """
         # Only add the publish button if the content type is registered
         # with versioning
         if not self._is_versioned():
             return
-        # Only add the publish button if the version is a draft
-        content_type = ContentType.objects.get_for_model(self.toolbar.obj)
-        version = Version.objects.get(
-            content_type=content_type, object_id=self.toolbar.obj.pk)
-        if version.state != DRAFT:
+        # Add the publish button if in edit mode
+        if self.toolbar.edit_mode_active:
+            item = ButtonList(side=self.toolbar.RIGHT)
+            proxy_model = self._get_proxy_model()
+            version = Version.objects.get_for_content(self.toolbar.obj)
+            publish_url = reverse('admin:{app}_{model}_publish'.format(
+                app=proxy_model._meta.app_label,
+                model=proxy_model.__name__.lower(),
+            ), args=(version.pk,))
+            item.add_button(
+                _('Publish'),
+                url=publish_url,
+                disabled=False,
+                extra_classes=['cms-btn-action'],
+            )
+            self.toolbar.add_item(item)
+
+    def _add_edit_button(self):
+        """Helper method to add an edit button to the toolbar
+        """
+        # Only add the edit button if the content type is registered
+        # with versioning
+        if not self._is_versioned():
             return
-        # Add the publish button in all other cases
-        proxy_model = self._get_proxy_model()
-        publish_url = reverse('admin:{app}_{model}_publish'.format(
-            app=proxy_model._meta.app_label,
-            model=proxy_model.__name__.lower(),
-        ), args=(self.toolbar.obj.pk,))
-        item.add_button(
-            _('Publish'),
-            url=publish_url,
-            disabled=False,
-        )
+        # Add the edit button if in preview mode
+        if self.toolbar.content_mode_active:
+            item = ButtonList(side=self.toolbar.RIGHT)
+            proxy_model = self._get_proxy_model()
+            version = Version.objects.get_for_content(self.toolbar.obj)
+            edit_url = reverse('admin:{app}_{model}_edit_redirect'.format(
+                app=proxy_model._meta.app_label,
+                model=proxy_model.__name__.lower(),
+            ), args=(version.pk,))
+            item.add_button(
+                _('Edit'),
+                url=edit_url,
+                disabled=False,
+                extra_classes=['cms-btn-action'],
+            )
+            self.toolbar.add_item(item)
 
     def post_template_populate(self):
         super(VersioningToolbar, self).post_template_populate()
-        # Create a button area on the toolbar
-        item = ButtonList(side=self.toolbar.RIGHT)
-        self.toolbar.add_item(item)
-        # Add buttons
-        self._add_publish_button(item)
+        self._add_edit_button()
+        self._add_publish_button()
--- tests/test_models.py
+++ tests/test_models.py
@@ -7,8 +7,9 @@ from cms.test_utils.testcases import CMSTestCase
 
 from freezegun import freeze_time
 
-from djangocms_versioning.constants import DRAFT
+from djangocms_versioning.constants import DRAFT, PUBLISHED
 from djangocms_versioning.datastructures import VersionableItem, default_copy
+from djangocms_versioning.models import Version, VersionQuerySet
 from djangocms_versioning.test_utils import factories
 from djangocms_versioning.test_utils.polls.cms_config import PollsCMSConfig
 from djangocms_versioning.test_utils.polls.models import PollContent
@@ -34,7 +35,7 @@ class CopyTestCase(CMSTestCase):
         """
         with freeze_time('2017-07-07'):
             # Make sure created in the past
-            original_version = factories.PollVersionFactory()
+            original_version = factories.PollVersionFactory(state=PUBLISHED)
         user = factories.UserFactory()
 
         new_version = original_version.copy(user)
@@ -43,6 +44,8 @@ class CopyTestCase(CMSTestCase):
         self.assertNotEqual(original_version.pk, new_version.pk)
         self.assertEqual(new_version.created, now())
         self.assertEqual(new_version.created_by, user)
+        # The state should always be DRAFT no matter what the original
+        # state was
         self.assertEqual(new_version.state, DRAFT)
 
     def test_content_object_gets_duplicated_with_default_copy(self):
@@ -107,3 +110,19 @@ class TestVersionModelProperties(CMSTestCase):
     def test_grouper(self):
         version = factories.PollVersionFactory()
         self.assertEqual(version.grouper, version.content.poll)
+
+
+class TestVersionQuerySet(CMSTestCase):
+
+    def test_version_uses_versionqueryset_as_manager(self):
+        self.assertEqual(
+            Version.objects._queryset_class,
+            VersionQuerySet,
+        )
+
+    def test_get_for_content(self):
+        version = factories.PollVersionFactory()
+        self.assertEqual(
+            Version.objects.get_for_content(version.content),
+            version,
+        )

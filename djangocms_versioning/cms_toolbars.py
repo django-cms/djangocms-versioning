a7ec2099306d430996714d81aa0b79d97e64d63e
Krzysztof Socha <k.socha@merixstudio.com>
2018-08-20 17:50:34 +0200

    Remove unused import

--- djangocms_versioning/cms_toolbars.py
+++ djangocms_versioning/cms_toolbars.py
@@ -1,5 +1,4 @@
 from django.apps import apps
-from django.contrib.contenttypes.models import ContentType
 from django.core.urlresolvers import reverse
 from django.utils.translation import ugettext_lazy as _
 

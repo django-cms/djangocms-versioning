b18291217a3a55049bceacd14e5a2060d2008825
Monika Sulik <kontakt@monikasulik.net>
2018-08-22 14:02:56 +0100

    Register more test models in admin for demo purposes

--- djangocms_versioning/test_utils/polls/admin.py
+++ djangocms_versioning/test_utils/polls/admin.py
@@ -1,8 +1,19 @@
 from django.contrib import admin
 
-from .models import PollContent
+from .models import PollContent, Poll, Answer
 
 
 @admin.register(PollContent)
 class PollContentAdmin(admin.ModelAdmin):
     pass
+
+
+@admin.register(Poll)
+class PollAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Answer)
+class AnswerAdmin(admin.ModelAdmin):
+    pass
+

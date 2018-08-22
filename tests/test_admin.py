ef75c84461d06f10bb753ae542eff5eb5c96db7b
monikasulik <kontakt@monikasulik.net>
2018-08-22 12:01:28 +0100

    Disallow editing content objects which are not drafts (#46)

--- tests/test_admin.py
+++ tests/test_admin.py
@@ -1184,3 +1184,64 @@ class VersionChangeListTestCase(CMSTestCase):
             transform=lambda x: x.pk,
             ordered=False
         )
+
+
+class VersionChangeViewTestCase(CMSTestCase):
+
+    def setUp(self):
+        self.versionable = PollsCMSConfig.versioning[0]
+        self.staff_user = self.get_superuser()
+
+    def test_change_view_returns_200_for_draft(self):
+        content = factories.PollContentWithVersionFactory(
+            version__state=constants.DRAFT)
+        url = self.get_admin_url(PollContent, 'change', content.pk)
+
+        with self.login_user_context(self.staff_user):
+            response = self.client.get(url)
+
+        self.assertEqual(response.status_code, 200)
+
+    def test_change_view_returns_404_for_published(self):
+        content = factories.PollContentWithVersionFactory(
+            version__state=constants.PUBLISHED)
+        url = self.get_admin_url(PollContent, 'change', content.pk)
+
+        with self.login_user_context(self.staff_user):
+            response = self.client.get(url)
+
+        self.assertEqual(response.status_code, 404)
+
+    def test_change_view_returns_404_for_unpublished(self):
+        content = factories.PollContentWithVersionFactory(
+            version__state=constants.UNPUBLISHED)
+        url = self.get_admin_url(PollContent, 'change', content.pk)
+
+        with self.login_user_context(self.staff_user):
+            response = self.client.get(url)
+
+        self.assertEqual(response.status_code, 404)
+
+    def test_change_view_returns_404_for_archived(self):
+        content = factories.PollContentWithVersionFactory(
+            version__state=constants.ARCHIVED)
+        url = self.get_admin_url(PollContent, 'change', content.pk)
+
+        with self.login_user_context(self.staff_user):
+            response = self.client.get(url)
+
+        self.assertEqual(response.status_code, 404)
+
+    @patch('django.contrib.messages.add_message')
+    def test_change_view_redirects_for_nonexistent_object(self, mocked_messages):
+        url = self.get_admin_url(PollContent, 'change', 144)
+
+        with self.login_user_context(self.staff_user):
+            response = self.client.get(url)
+
+        self.assertRedirects(response, '/en/admin/', target_status_code=302)
+        self.assertEqual(mocked_messages.call_count, 1)
+        self.assertEqual(mocked_messages.call_args[0][1], 30)  # warning level
+        self.assertEqual(
+            mocked_messages.call_args[0][2],
+            'poll content with ID "144" doesn\'t exist. Perhaps it was deleted?')

8d0b38581f2c6ad16bc05f511c1a9e879f42f3c4
Aiky30 <aiky30@yahoo.com>
2018-09-07 12:01:04 +0100

    Added tests for the grouper selector format

--- tests/test_forms.py
+++ tests/test_forms.py
@@ -1,5 +1,6 @@
 from django import forms
 
+from cms.models import PageContent
 from cms.test_utils.testcases import CMSTestCase
 
 from djangocms_versioning.forms import grouper_form_factory
@@ -30,4 +31,32 @@ class GrouperFormTestCase(CMSTestCase):
         self.assertEqual(
             id(grouper_form_factory(PollContent, language='en')),
             id(grouper_form_factory(PollContent, language='en')),
+        )
+
+    def test_grouper_selector_default_label(self):
+        """
+        Grouper selector shows the default label format when no override is set
+        """
+        version = factories.PollVersionFactory()
+        form_class = grouper_form_factory(PollContent, language='en')
+
+        self.assertIn(
+            (version.content.poll.pk, str(version.content.poll)),
+            form_class.base_fields['grouper'].choices,
+        )
+
+    def test_grouper_selector_non_default_label(self):
+        """
+        Grouper selector shows the PageContent label format when PageContent is set
+        """
+        version = factories.PageVersionFactory()
+        form_class = grouper_form_factory(PageContent, version.content.language)
+        label = "{title} ({path})".format(
+            title=version.content.page.get_title(version.content.language),
+            path=version.content.page.get_path(version.content.language),
+        )
+
+        self.assertIn(
+            (version.content.page.pk, label),
+            form_class.base_fields['grouper'].choices,
         )

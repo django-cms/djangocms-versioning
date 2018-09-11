5a5572847bc6e5a9ee1d979fcd954403179740e6
Krzysztof Socha <k.socha@merixstudio.com>
2018-09-11 12:31:08 +0200

    Use create_user instead of create in user factory to store hashed password

--- djangocms_versioning/test_utils/factories.py
+++ djangocms_versioning/test_utils/factories.py
@@ -25,6 +25,13 @@ class UserFactory(factory.django.DjangoModelFactory):
     class Meta:
         model = User
 
+    @classmethod
+    def _create(cls, model_class, *args, **kwargs):
+        """Override the default ``_create`` with our custom call."""
+        manager = cls._get_manager(model_class)
+        # The default would use ``manager.create(*args, **kwargs)``
+        return manager.create_user(*args, **kwargs)
+
 
 class AbstractVersionFactory(factory.DjangoModelFactory):
     object_id = factory.SelfAttribute('content.id')

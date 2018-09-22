b24cd4793fcaeca662de06fc1fd51ff0153a0d3d
Damilare Onajole <web.dami@gmail.com>
2018-09-22 09:09:59 +0100

    isorted

--- djangocms_versioning/cms_config.py
+++ djangocms_versioning/cms_config.py
@@ -6,8 +6,8 @@ from django.utils.functional import cached_property
 
 from cms.app_base import CMSAppConfig, CMSAppExtension
 from cms.models import PageContent, Placeholder
-from cms.signals import post_obj_operation
 from cms.operations import CHANGE_PAGE
+from cms.signals import post_obj_operation
 
 from .datastructures import VersionableItem
 from .helpers import (

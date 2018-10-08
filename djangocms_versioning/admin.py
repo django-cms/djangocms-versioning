b8267b2919e10714ea4d203d0d8559a09873f3a6
Krzysztof Socha <k.socha@merixstudio.com>
2018-10-08 12:36:51 +0200

    Return a list from get_list_filter

--- djangocms_versioning/admin.py
+++ djangocms_versioning/admin.py
@@ -152,10 +152,10 @@ class VersionAdmin(admin.ModelAdmin):
 
     def get_list_filter(self, request):
         versionable = versionables.for_content(self.model._source_model)
-        return (
+        return [
             fake_filter_factory(versionable, field)
             for field in versionable.extra_grouping_fields
-        )
+        ]
 
     def _state_actions(self, request):
         def state_actions(obj):

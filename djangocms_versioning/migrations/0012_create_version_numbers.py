0064f7ecc2e26ed386e41c66341f3ea7be25dc58
Aiky30 <aiky30@yahoo.com>
2018-10-08 16:39:55 +0100

    Amended previous hack and now have something working although not for a page with  multiple langauges, distinct will get the first found

--- djangocms_versioning/migrations/0012_create_version_numbers.py
+++ djangocms_versioning/migrations/0012_create_version_numbers.py
@@ -15,71 +15,32 @@ def forwards(app_registry, schema_editor):
     Version = app_registry.get_model('djangocms_versioning', 'Version')
     ContentType = apps.get_model('contenttypes', 'ContentType')
 
-    """
-    Look at how the grouper admin selector gets the list
-    from that list you should then be able to query the versions by filter_by_grouping_fields
-    
-    # Get all objects for grouper
-    all_versions_for_grouper = versionable.for_grouper(versionable.grouper_field)
-
-    # For each possible combination of grouping values
-
-
-    grouping_values = versionable.grouping_values(self.content)
-
-    fields = list(versionable.grouping_fields)
-
-    content_type = ContentType.objects.get_for_model(versionable.content_model)
-    content_objects = versionable.for_grouping_values(fields)
-
-    #versionable.grouping_values(versionable.version_model_proxy.content)
-
-    version_list = Version.objects.filter_by_grouping_fields(
-        versionable, **versionable.grouping_fields
-    ).all()
-    
-    """
-
     # For each registered version model
     for versionable in versioning_extension.versionables:
 
         distinct_groupers = versionable.distinct_groupers().all()
 
-        # For each grouper
+        #groupers = versionable.content_model._base_manager.all()
+
         # FIXME: No good as it doesn't provide pages with different languages!!
-        for grouper_content in distinct_groupers:
+        # TODO: Requries a query that fetches all groupers with all grouping fields applied
+        for content in distinct_groupers:
 
-            fields = versionable.grouping_values(grouper_content)
-
+            fields = versionable.grouping_values(content)
             content_objects = versionable.for_grouping_values(**fields)
             content_type = ContentType.objects.get_for_model(versionable.content_model)
 
             version_list = Version.objects.filter(
                 object_id__in=content_objects,
-                content_type=content_type,
-            )
+                content_type_id=content_type.pk,
+            ).order_by('pk')
 
-
-            """
-            version_list = Version.objects.filter_by_grouping_fields(
-                versionable, **fields
-            ).all()
-            
-  
-            grouper_field = versionable.grouper_model
-            content_for_grouper = versionable.for_grouper(grouper_field)
-
-            # Get the content for each grouper using the grouping fields
-            unique_grouper = versionable.for_content_grouping_values(grouper_content).order_by('pk')
-
-            number = 0
-            for version in content_versions:
-                number = number + 1
-                version_number = version.number
-                version.number = number
+            previous_number = 0
+            for version in version_list:
+                version.number = previous_number + 1
+                previous_number = version.number
+                version.save()
                 continue
-            """
-            continue
 
     raise Exception('Migration prevented for dev')
 

05052a144da8da9b08601d511a86f8108b10713e
Rizwan <rizwan@webbyfox.com>
2018-10-09 10:38:26 +0100

    Added archive, revert and unpublish confirmation screen on action (#114)

--- djangocms_versioning/admin.py
+++ djangocms_versioning/admin.py
@@ -1,5 +1,5 @@
 from django.conf.urls import url
-from django.contrib import admin, messages
+from django.contrib import admin
 from django.contrib.admin.options import (
     TO_FIELD_VAR,
     IncorrectLookupParameters,
@@ -304,15 +304,18 @@ class VersionAdmin(admin.ModelAdmin):
             revert_url = ''
         else:
             disable = False
-            revert_url = reverse('admin:{app}_{model}_revert'.format(
-                app=obj._meta.app_label, model=self.model._meta.model_name,
-            ), args=(obj.pk,))
+            revert_url = reverse(
+                'admin:{app}_{model}_revert'.format(
+                    app=obj._meta.app_label,
+                    model=self.model._meta.model_name,
+                    ),
+                args=(obj.pk,))
 
         return render_to_string(
             'djangocms_versioning/admin/revert_icon.html',
             {
                 'revert_url': revert_url,
-                'disable': disable
+                'disable': disable,
             }
         )
 
@@ -363,9 +366,6 @@ class VersionAdmin(admin.ModelAdmin):
         """Archives the specified version and redirects back to the
         version changelist
         """
-        # This view always changes data so only POST requests should work
-        if request.method != 'POST':
-            return HttpResponseNotAllowed(['POST'], _('This view only supports POST method.'))
 
         # Check version exists
         version = self.get_object(request, unquote(object_id))
@@ -375,10 +375,25 @@ class VersionAdmin(admin.ModelAdmin):
         # Raise 404 if not in draft status
         if version.state != DRAFT:
             raise Http404
-        # Archive the version
-        version.archive(request.user)
-        # Display message
-        messages.success(request, _("Version archived"))
+
+        if request.method != 'POST':
+            context = dict(
+                object_name=version.content,
+                object_id=object_id,
+                archive_url=reverse(
+                    'admin:{app}_{model}_archive'.format(
+                        app=self.model._meta.app_label,
+                        model=self.model._meta.model_name,
+                    ),
+                    args=(version.content.pk,)),
+                back_url=version_list_url(version.content),
+            )
+            return render(request, 'djangocms_versioning/admin/archive_confirmation.html', context)
+        else:
+            # Archive the version
+            version.archive(request.user)
+            # Display message
+            self.message_user(request, _("Version archived"))
         # Redirect
         return redirect(version_list_url(version.content))
 
@@ -401,7 +416,7 @@ class VersionAdmin(admin.ModelAdmin):
         # Publish the version
         version.publish(request.user)
         # Display message
-        messages.success(request, _("Version published"))
+        self.message_user(request, _("Version published"))
         # Redirect
         return redirect(version_list_url(version.content))
 
@@ -409,10 +424,6 @@ class VersionAdmin(admin.ModelAdmin):
         """Unpublishes the specified version and redirects back to the
         version changelist
         """
-        # This view always changes data so only POST requests should work
-        if request.method != 'POST':
-            return HttpResponseNotAllowed(['POST'], _('This view only supports POST method.'))
-
         # Check version exists
         version = self.get_object(request, unquote(object_id))
         if version is None:
@@ -421,10 +432,25 @@ class VersionAdmin(admin.ModelAdmin):
         # Raise 404 if not in published status
         if version.state != PUBLISHED:
             raise Http404
-        # Unpublish the version
-        version.unpublish(request.user)
-        # Display message
-        messages.success(request, _("Version unpublished"))
+        # This view always changes data so only POST requests should work
+        if request.method != 'POST':
+            context = dict(
+                object_name=version.content,
+                object_id=object_id,
+                unpublish_url=reverse(
+                    'admin:{app}_{model}_unpublish'.format(
+                        app=self.model._meta.app_label,
+                        model=self.model._meta.model_name,
+                    ),
+                    args=(version.content.pk,)),
+                back_url=version_list_url(version.content),
+            )
+            return render(request, 'djangocms_versioning/admin/unpublish_confirmation.html', context)
+        else:
+            # Unpublish the version
+            version.unpublish(request.user)
+            # Display message
+            self.message_user(request, _("Version unpublished"))
         # Redirect
         return redirect(version_list_url(version.content))
 
@@ -476,13 +502,15 @@ class VersionAdmin(admin.ModelAdmin):
         if no draft exists yet.
         """
         version = self.get_object(request, unquote(object_id))
-
         if version is None:
+            raise Http404
+
+        if version.state not in (UNPUBLISHED, ARCHIVED):
+            # if version state not unpublished or archived then raise 404
             raise Http404
 
         pks_for_grouper = version.versionable.for_content_grouping_values(
             version.content).values_list('pk', flat=True)
-
         drafts = Version.objects.filter(
             object_id__in=pks_for_grouper, content_type=version.content_type,
             state=DRAFT)
@@ -492,13 +520,23 @@ class VersionAdmin(admin.ModelAdmin):
             # should raise 404.
             raise Http404
 
-        if version.state not in (UNPUBLISHED, ARCHIVED):
-            # if version state not unpublished or archived then raise 404
-            raise Http404
-
-        version = version.copy(request.user)
-        # Redirect
-        return redirect(version_list_url(version.content))
+        if request.method != 'POST':
+            context = dict(
+                object_name=version.content,
+                object_id=object_id,
+                revert_url=reverse(
+                    'admin:{app}_{model}_revert'.format(
+                        app=self.model._meta.app_label,
+                        model=self.model._meta.model_name,
+                        ),
+                    args=(version.content.pk,)),
+                back_url=version_list_url(version.content),
+            )
+            return render(request, 'djangocms_versioning/admin/revert_confirmation.html', context)
+        else:
+            version = version.copy(request.user)
+            # Redirect
+            return redirect(version_list_url(version.content))
 
     def compare_view(self, request, object_id):
         """Compares two versions
--- tests/test_admin.py
+++ tests/test_admin.py
@@ -397,7 +397,9 @@ class VersionAdminActionsTestCase(CMSTestCase):
         actual_enabled_control = self.version_admin._get_revert_link(version, request)
 
         expected_enabled_state = (
-            '<a class="btn cms-versioning-action-btn js-versioning-action js-versioning-keep-sideframe" href="%s" '
+            '<a class="btn cms-form-get-method cms-versioning-action-btn js-versioning-action '
+            'js-versioning-keep-sideframe" '
+            'href="%s" '
             'title="Revert">'
         ) % draft_edit_url
         self.assertIn(expected_enabled_state, actual_enabled_control.replace('\n', ''))
@@ -775,7 +777,7 @@ class ArchiveViewTestCase(CMSTestCase):
         self.assertEqual(StateTracking.objects.all().count(), 0)
 
     @freeze_time(None)
-    @patch('django.contrib.messages.success')
+    @patch('django.contrib.admin.ModelAdmin.message_user')
     def test_archive_view_sets_state_and_redirects(self, mocked_messages):
         poll_version = factories.PollVersionFactory(state=constants.DRAFT)
         url = self.get_admin_url(
@@ -874,7 +876,7 @@ class ArchiveViewTestCase(CMSTestCase):
             mocked_messages.call_args[0][2],
             'poll content version with ID "89" doesn\'t exist. Perhaps it was deleted?')
 
-    def test_archive_view_cant_be_accessed_by_get_request(self):
+    def test_archive_view_can_be_accessed_by_get_request(self):
         poll_version = factories.PollVersionFactory(state=constants.DRAFT)
         url = self.get_admin_url(
             self.versionable.version_model_proxy, 'archive', poll_version.pk)
@@ -882,9 +884,8 @@ class ArchiveViewTestCase(CMSTestCase):
         with self.login_user_context(self.get_staff_user_with_no_permissions()):
             response = self.client.get(url)
 
-        self.assertEqual(response.status_code, 405)
-        self.assertEqual(response._headers.get('allow'), ('Allow', 'POST'))
-        # status hasn't changed
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.request['REQUEST_METHOD'], 'GET')
         poll_version_ = Version.objects.get(pk=poll_version.pk)
         self.assertEqual(poll_version_.state, constants.DRAFT)
         # no status change has been tracked
@@ -911,7 +912,7 @@ class PublishViewTestCase(CMSTestCase):
         self.assertEqual(StateTracking.objects.all().count(), 0)
 
     @freeze_time(None)
-    @patch('django.contrib.messages.success')
+    @patch('django.contrib.admin.ModelAdmin.message_user')
     def test_publish_view_sets_state_and_redirects(self, mocked_messages):
         poll_version = factories.PollVersionFactory(state=constants.DRAFT)
         url = self.get_admin_url(
@@ -1047,7 +1048,7 @@ class UnpublishViewTestCase(CMSTestCase):
         self.assertEqual(StateTracking.objects.all().count(), 0)
 
     @freeze_time(None)
-    @patch('django.contrib.messages.success')
+    @patch('django.contrib.admin.ModelAdmin.message_user')
     def test_unpublish_view_sets_state_and_redirects(self, mocked_messages):
         poll_version = factories.PollVersionFactory(state=constants.PUBLISHED)
         url = self.get_admin_url(
@@ -1143,7 +1144,7 @@ class UnpublishViewTestCase(CMSTestCase):
             mocked_messages.call_args[0][2],
             'poll content version with ID "89" doesn\'t exist. Perhaps it was deleted?')
 
-    def test_unpublish_view_cant_be_accessed_by_get_request(self):
+    def test_unpublish_view_can_be_accessed_by_get_request(self):
         poll_version = factories.PollVersionFactory(state=constants.PUBLISHED)
         url = self.get_admin_url(
             self.versionable.version_model_proxy, 'unpublish', poll_version.pk)
@@ -1151,8 +1152,8 @@ class UnpublishViewTestCase(CMSTestCase):
         with self.login_user_context(self.get_staff_user_with_no_permissions()):
             response = self.client.get(url)
 
-        self.assertEqual(response.status_code, 405)
-        self.assertEqual(response._headers.get('allow'), ('Allow', 'POST'))
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.request['REQUEST_METHOD'], 'GET')
         # status hasn't changed
         poll_version_ = Version.objects.get(pk=poll_version.pk)
         self.assertEqual(poll_version_.state, constants.PUBLISHED)

e47554687772330681a9ac0eef04a2b0681d7177
Damilare Onajole <web.dami@gmail.com>
2018-10-25 14:47:24 +0100

    a bit of a refactor

--- djangocms_versioning/cms_config.py
+++ djangocms_versioning/cms_config.py
@@ -115,16 +115,16 @@ class VersioningCMSExtension(CMSAppExtension):
 
 def copy_page_content(original_content):
     """Copy the PageContent object and deepcopy its
-    placeholders and plugins
+    placeholders and plugins. Don't copy the primary key
+    because we are creating a new obj, and also the
+    creation_date as we want copied content to reflect
+    the date it was copied.
     """
     # Copy content object
     content_fields = {
         field.name: getattr(original_content, field.name)
         for field in PageContent._meta.fields
-        # don't copy primary key because we're creating a new obj
-        if PageContent._meta.pk.name != field.name
-        # don't copy date
-        and field.name != 'creation_date'
+        if field.name not in (PageContent._meta.pk.name, 'creation_date')
     }
 
     new_content = PageContent.objects.create(**content_fields)

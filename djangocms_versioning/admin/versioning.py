5b392caf5365e838e620bdf0e44016e53dbe0e6b
Jonathan Sundqvist <sundqvist.jonathan@gmail.com>
2020-03-20 12:00:50 +0100

    Move original admin

--- djangocms_versioning/admin/versioning.py
+++ djangocms_versioning/admin/versioning.py
@@ -3,7 +3,7 @@ from collections import OrderedDict
 from django.conf.urls import url
 from django.contrib import admin, messages
 from django.contrib.admin.options import IncorrectLookupParameters
-from django.contrib.admin.utils import flatten_fieldsets, unquote
+from django.contrib.admin.utils import unquote
 from django.contrib.admin.views.main import ChangeList
 from django.contrib.contenttypes.models import ContentType
 from django.core.exceptions import ObjectDoesNotExist
@@ -16,113 +16,17 @@ from django.utils.encoding import force_text
 from django.utils.html import format_html, format_html_join
 from django.utils.translation import ugettext_lazy as _
 
-from cms.models import PageContent
 from cms.utils import get_language_from_request
 from cms.utils.conf import get_cms_setting
 from cms.utils.urlutils import add_url_parameters
 
-from . import versionables
-from .compat import DJANGO_GTE_21
-from .constants import ARCHIVED, DRAFT, PUBLISHED, UNPUBLISHED
-from .exceptions import ConditionFailed
-from .forms import grouper_form_factory
-from .helpers import get_admin_url, get_editable_url, get_preview_url, version_list_url
-from .models import Version
-from .versionables import _cms_extension
-
-
-class VersioningChangeListMixin:
-    """Mixin used for ChangeList classes of content models."""
-
-    def get_queryset(self, request):
-        """Limit the content model queryset to latest versions only."""
-        queryset = super().get_queryset(request)
-        versionable = versionables.for_content(queryset.model)
-        return queryset.filter(pk__in=versionable.distinct_groupers())
-
-
-def versioning_change_list_factory(base_changelist_cls):
-    """Generate a ChangeList class to use for the content model"""
-    return type(
-        "Versioned" + base_changelist_cls.__name__,
-        (VersioningChangeListMixin, base_changelist_cls),
-        {}
-    )
-
-
-class VersioningAdminMixin:
-    """Mixin providing versioning functionality to admin classes of
-    content models.
-    """
-
-    def save_model(self, request, obj, form, change):
-        """
-        Overrides the save method to create a version object
-        when a content object is created
-        """
-        super().save_model(request, obj, form, change)
-        if not change:
-            if isinstance(obj, PageContent):
-                # FIXME disabled version creation for `cms.PageContent`
-                # here, as it's already done in `cms.api.create_title`
-                return
-            # create a new version object and save it
-            Version.objects.create(content=obj, created_by=request.user)
-
-    def get_queryset(self, request):
-        """Override manager so records not in published state can be displayed"""
-        from .helpers import override_default_manager
-
-        with override_default_manager(self.model, self.model._original_manager):
-            queryset = super().get_queryset(request)
-        return queryset
-
-    def get_changelist(self, request, **kwargs):
-        ChangeList = super().get_changelist(request, **kwargs)
-        return versioning_change_list_factory(ChangeList)
-
-    change_form_template = "djangocms_versioning/admin/mixin/change_form.html"
-
-    def render_change_form(
-        self, request, context, add=False, change=False, form_url="", obj=None
-    ):
-        """Add a link to the version table to the change form view"""
-        if "versioning_fallback_change_form_template" not in context:
-            context[
-                "versioning_fallback_change_form_template"
-            ] = super().change_form_template
-
-        return super().render_change_form(request, context, add, change, form_url, obj)
-
-    def get_readonly_fields(self, request, obj=None):
-        """Port permission code from django >= 2.1.
-
-        In later versions of django if a user has view perms but no
-        change perms, fields are set as read-only. This is not the case
-        in django < 2.1.
-        """
-        if obj and not DJANGO_GTE_21:
-            version = Version.objects.get_for_content(obj)
-            if not version.check_modify.as_bool(request.user):
-                if self.fields:
-                    return self.fields
-                if self.fieldsets:
-                    return flatten_fieldsets(self.fieldsets)
-                if self.form.declared_fields:
-                    return self.form.declared_fields
-                return list(
-                    set(
-                        [field.name for field in self.opts.local_fields]
-                        + [field.name for field in self.opts.local_many_to_many]
-                    )
-                )
-        return super().get_readonly_fields(request, obj)
-
-    def has_change_permission(self, request, obj=None):
-        if obj and DJANGO_GTE_21:
-            version = Version.objects.get_for_content(obj)
-            return version.check_modify.as_bool(request.user)
-        return super().has_change_permission(request, obj)
+from .. import versionables
+from ..constants import ARCHIVED, DRAFT, PUBLISHED, UNPUBLISHED
+from ..exceptions import ConditionFailed
+from ..forms import grouper_form_factory
+from ..helpers import get_admin_url, get_editable_url, get_preview_url, version_list_url
+from ..models import Version
+from ..versionables import _cms_extension
 
 
 class VersionChangeList(ChangeList):

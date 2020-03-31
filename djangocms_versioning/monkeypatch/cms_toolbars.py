aa177dd850b150b80470a8bed76dcb7f3bf352de
Jonathan Sundqvist <sundqvist.jonathan@gmail.com>
2020-03-31 09:48:20 +0200

    Update the change_language_menu to most recent version
    The change_language_menu is taken from the most recent version
    in django-cms. In essence we don't need to monkeypatch this,
    right now we still need this monkeypatch because the work to
    overwrite the delete view in pagecontent admin is not yet done.
    
    Once that work is done this monkeypatch is redundant.

--- djangocms_versioning/monkeypatch/cms_toolbars.py
+++ djangocms_versioning/monkeypatch/cms_toolbars.py
@@ -13,7 +13,9 @@ from cms.utils.urlutils import add_url_parameters, admin_reverse
 def change_language_menu(self):
     if self.toolbar.edit_mode_active and self.page:
         can_change = page_permissions.user_can_change_page(
-            user=self.request.user, page=self.page, site=self.current_site
+            user=self.request.user,
+            page=self.page,
+            site=self.current_site,
         )
     else:
         can_change = False
@@ -25,27 +27,49 @@ def change_language_menu(self):
 
         languages = get_language_dict(self.current_site.pk)
 
-        remove = [
-            (code, languages.get(code, code))
-            for code in self.page.get_languages()
-            if code in languages
-        ]
+        remove = [(code, languages.get(code, code)) for code in self.page.get_languages() if code in languages]
         add = [l for l in languages.items() if l not in remove]
+        copy = [(code, name) for code, name in languages.items() if code != self.current_lang and (code, name) in remove]
 
-        if add:
+        if add or remove or copy:
             language_menu.add_break(ADD_PAGE_LANGUAGE_BREAK)
 
-            add_plugins_menu = language_menu.get_or_create_menu(
-                "{0}-add".format(LANGUAGE_MENU_IDENTIFIER), _("Add Translation")
-            )
+        if add:
+            add_plugins_menu = language_menu.get_or_create_menu('{0}-add'.format(LANGUAGE_MENU_IDENTIFIER), _('Add Translation'))
 
-            page_add_url = admin_reverse("cms_pagecontent_add")
+            page_add_url = admin_reverse('cms_pagecontent_add')
 
             for code, name in add:
-                url = add_url_parameters(
-                    page_add_url, cms_page=self.page.pk, language=code
-                )
+                url = add_url_parameters(page_add_url, cms_page=self.page.pk, language=code)
                 add_plugins_menu.add_modal_item(name, url=url)
+
+        # TODO to enable removal urls you need to overwrite the delete view so that pagecontent is unpublished
+        # instead of deleted. Once this work is done the `change_language_menu` monkeypatch can be
+        # removed entirely.
+
+        # if remove:
+        #     translation_delete_url = admin_reverse('cms_pagecontent_delete', args=(self.title.pk,))
+
+        #     remove_plugins_menu = language_menu.get_or_create_menu('{0}-del'.format(LANGUAGE_MENU_IDENTIFIER), _('Delete Translation'))
+        #     disabled = len(remove) == 1
+        #     for code, name in remove:
+        #         url = add_url_parameters(translation_delete_url, language=code)
+        #         remove_plugins_menu.add_modal_item(name, url=url, disabled=disabled)
+
+        if copy:
+            copy_plugins_menu = language_menu.get_or_create_menu('{0}-copy'.format(LANGUAGE_MENU_IDENTIFIER), _('Copy all plugins'))
+            title = _('from %s')
+            question = _('Are you sure you want to copy all plugins from %s?')
+
+            page_copy_url = admin_reverse('cms_pagecontent_copy_language', args=(self.title.pk,))
+
+            for code, name in copy:
+                copy_plugins_menu.add_ajax_item(
+                    title % name, action=page_copy_url,
+                    data={'source_language': code, 'target_language': self.current_lang},
+                    question=question % name, on_success=self.toolbar.REFRESH_PAGE
+                )
+
 
 
 PageToolbar.change_language_menu = change_language_menu  # noqa: E305

38c8f9ba3f1a4bc1db243a2708633d4d1da07ef9
Adam Murray <adam-murray@hotmail.co.uk>
2021-08-05 16:17:17 +0100

    Fixed language issue when generating PollContent from factory in admin tests

--- tests/test_admin.py
+++ tests/test_admin.py
@@ -2415,7 +2415,7 @@ class VersionChangeViewTestCase(CMSTestCase):
 class ExtendedVersionAdminTestCase(CMSTestCase):
 
     def setup(self):
-        self.poll_content = factories.PollContentWithVersionFactory()
+        self.poll_content = factories.PollContentWithVersionFactory(PollContentFactory())
 
     def test_extended_version_change_list_display_renders_from_cms_config(self):
         """
@@ -2423,7 +2423,7 @@ class ExtendedVersionAdminTestCase(CMSTestCase):
         populated from cms_config are rendered in change_list
         """
         poll = factories.PollFactory()
-        factories.PollContentWithVersionFactory()
+        factories.PollContentFactory(poll=poll, language="en")
         factories.PollVersionFactory(content__poll=poll)
 
         with self.login_user_context(self.get_superuser()):
@@ -2431,6 +2431,7 @@ class ExtendedVersionAdminTestCase(CMSTestCase):
 
         # Check response is valid
         self.assertEqual(200, response.status_code)
+
         # Check list_display item from cms_config is rendered
         self.assertContains(response, '<a href="?o=1">Text</a></div>')
         # Check list_action links are rendered

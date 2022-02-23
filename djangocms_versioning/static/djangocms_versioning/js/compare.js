27368a627cf115ea3dfde172d31cee71c9b67444
bernard <bvdv9075@gmail.com>
2022-02-23 13:23:01 +0000

    Added event to reload page on button click

--- djangocms_versioning/static/djangocms_versioning/js/compare.js
+++ djangocms_versioning/static/djangocms_versioning/js/compare.js
@@ -2,8 +2,10 @@ window.addEventListener('load', function() {
   document.querySelector('select[name=action]').addEventListener('change', function() {
     var selectVal = this.options[this.selectedIndex].text;
     if (selectVal == "Compare versions"){
+        // Setting the target to blank to ensure the compare view is opened in a new tab
         window.onload = document.getElementById('changelist-form').setAttribute('target', '_blank');
         var buttonElement = document.querySelector("button")
+        // Reloading the page to renew the csrf token which prevented multiple version compare tabs from opening
         if (buttonElement.innerHTML == "Go") {
             buttonElement.setAttribute("onclick", "window.location.reload();");
         }

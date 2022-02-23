314dc1beca09455119a52cb8a3166fd75099d0f7
bernard <bvdv9075@gmail.com>
2022-02-23 13:20:47 +0000

    Added event to reload page on button click

--- djangocms_versioning/static/djangocms_versioning/js/compare.js
+++ djangocms_versioning/static/djangocms_versioning/js/compare.js
@@ -4,7 +4,9 @@ window.addEventListener('load', function() {
     if (selectVal == "Compare versions"){
         window.onload = document.getElementById('changelist-form').setAttribute('target', '_blank');
         var buttonElement = document.querySelector("button")
-        buttonElement.setAttribute("onclick", "window.location.reload();");
+        if (buttonElement.innerHTML == "Go") {
+            buttonElement.setAttribute("onclick", "window.location.reload();");
+        }
     }
   })
 })

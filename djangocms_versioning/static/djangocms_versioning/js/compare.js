814803684589d631c774f7c650890c3ef38a9ac0
bernard <bvdv9075@gmail.com>
2022-02-28 18:00:38 +0000

    Reworked compare.js

--- djangocms_versioning/static/djangocms_versioning/js/compare.js
+++ djangocms_versioning/static/djangocms_versioning/js/compare.js
@@ -1,14 +1,15 @@
 window.addEventListener('load', function() {
   document.querySelector('select[name=action]').addEventListener('change', function() {
-    var selectVal = this.options[this.selectedIndex].text;
-    if (selectVal == "Compare versions"){
+    let selectVal = this.options[this.selectedIndex].getAttribute("value");
+    if (selectVal == "compare_versions") {
+        const actionsForm = document.getElementById('changelist-form')
         // Setting the target to blank to ensure the compare view is opened in a new tab
-        window.onload = document.getElementById('changelist-form').setAttribute('target', '_blank');
-        var buttonElement = document.querySelector("button")
+        actionsForm.setAttribute('target', '_blank');
+        const submitButton = actionsForm.querySelector('[type="submit"]')
         // Reloading the page to renew the csrf token which prevented multiple version compare tabs from opening
-        if (buttonElement.innerHTML == "Go") {
-            buttonElement.setAttribute("onclick", "window.location.reload();");
-        }
+        submitButton.addEventListener("click", function (e) {
+            window.location.reload()
+        });
     }
   })
 })

34ce3e128842a4939fa158261f049deb4bb3d519
bernard <bvdv9075@gmail.com>
2022-02-28 20:18:33 +0000

    Added else statement to remove target attribute if compare version is deselected

--- djangocms_versioning/static/djangocms_versioning/js/compare.js
+++ djangocms_versioning/static/djangocms_versioning/js/compare.js
@@ -1,8 +1,8 @@
 window.addEventListener('load', function() {
   document.querySelector('select[name=action]').addEventListener('change', function() {
     let selectVal = this.options[this.selectedIndex].getAttribute("value");
+    const actionsForm = document.getElementById('changelist-form')
     if (selectVal == "compare_versions") {
-        const actionsForm = document.getElementById('changelist-form')
         // Setting the target to blank to ensure the compare view is opened in a new tab
         actionsForm.setAttribute('target', '_blank');
         const submitButton = actionsForm.querySelector('[type="submit"]')
@@ -10,6 +10,9 @@ window.addEventListener('load', function() {
         submitButton.addEventListener("click", function (e) {
             window.location.reload()
         });
+    }else {
+        // If the user deselect the compare version action, the "target=_blank" is removed
+        actionsForm.removeAttribute('target');
     }
   })
 })

c111478e353b3cdab2316b5ba2fca655226def52
bernard <bvdv9075@gmail.com>
2022-03-18 14:19:50 +0000

    PR feedback addressed

--- djangocms_versioning/static/djangocms_versioning/js/messages.js
+++
@@ -1,7 +0,0 @@
-var djangoMessages = document.getElementsByClassName("messagelist");
-setTimeout(function(){
-    for (var i = 0; i < djangoMessages.length; i ++) {
-        djangoMessages[i].setAttribute('style', 'visibility: hidden; opacity: 0; ' +
-            'transition: visibility 0s linear 300ms, opacity 300ms; !important');
-    }
-}, 3000);

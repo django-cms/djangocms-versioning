69fb47cab1b45a5422374a10a8b6fe410a4998e8
Fabian Braun <fsbraun@gmx.de>
2022-11-08 23:33:53 +0100

    Only change migration for mysql

--- djangocms_versioning/migrations/0009_cms_pagecontent_remove_unique_constraint.py
+++ djangocms_versioning/migrations/0009_cms_pagecontent_remove_unique_constraint.py
@@ -1,34 +1,46 @@
 from __future__ import unicode_literals
 
-from django.db import migrations
+from django.db import connection, migrations
 
 
 def forwards(apps, schema_editor):
+
+    # Or alternatively
     PageContent = apps.get_model("cms", "PageContent")
     old_unique_together = PageContent._meta.unique_together
     new_unique_together = old_unique_together - {("language", "page")}
-    in_atomic_block = schema_editor.connection.in_atomic_block
-    schema_editor.connection.in_atomic_block = False
-    try:
+    if connection.settings_dict["ENGINE"].rsplit(".")[-1] == "mysql":
+        in_atomic_block = schema_editor.connection.in_atomic_block
+        schema_editor.connection.in_atomic_block = False
+        try:
+            schema_editor.alter_unique_together(
+                PageContent, old_unique_together, new_unique_together
+            )
+        finally:
+            schema_editor.connection.in_atomic_block = in_atomic_block
+    else:
         schema_editor.alter_unique_together(
             PageContent, old_unique_together, new_unique_together
         )
-    finally:
-        schema_editor.connection.in_atomic_block = in_atomic_block
 
 
 def backwards(apps, schema_editor):
     PageContent = apps.get_model("cms", "PageContent")
     old_unique_together = PageContent._meta.unique_together
     new_unique_together = old_unique_together | {("language", "page")}
-    in_atomic_block = schema_editor.connection.in_atomic_block
-    schema_editor.connection.in_atomic_block = False
-    try:
+    if connection.settings_dict["ENGINE"].rsplit(".")[-1] == "mysql":
+        in_atomic_block = schema_editor.connection.in_atomic_block
+        schema_editor.connection.in_atomic_block = False
+        try:
+            schema_editor.alter_unique_together(
+                PageContent, old_unique_together, new_unique_together
+            )
+        finally:
+            schema_editor.connection.in_atomic_block = in_atomic_block
+    else:
         schema_editor.alter_unique_together(
             PageContent, old_unique_together, new_unique_together
         )
-    finally:
-        schema_editor.connection.in_atomic_block = in_atomic_block
 
 
 class Migration(migrations.Migration):

79609b99f2f1b1c751bef8da4cec7b0d8196f4a5
Fabian Braun <fsbraun@gmx.de>
2022-11-08 18:48:46 +0100

    Undo changes to migration 0009

--- djangocms_versioning/migrations/0009_cms_pagecontent_remove_unique_constraint.py
+++ djangocms_versioning/migrations/0009_cms_pagecontent_remove_unique_constraint.py
@@ -7,14 +7,15 @@ def forwards(apps, schema_editor):
     PageContent = apps.get_model("cms", "PageContent")
     old_unique_together = PageContent._meta.unique_together
     new_unique_together = old_unique_together - {("language", "page")}
-    in_atomic_block = schema_editor.connection.in_atomic_block
+    # in_atomic_block = schema_editor.connection.in_atomic_block
     schema_editor.connection.in_atomic_block = False
     try:
         schema_editor.alter_unique_together(
             PageContent, old_unique_together, new_unique_together
         )
     finally:
-        schema_editor.connection.in_atomic_block = in_atomic_block
+        pass
+        # schema_editor.connection.in_atomic_block = in_atomic_block
 
 
 

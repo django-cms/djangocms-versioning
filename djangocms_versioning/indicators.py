4e5b1c47e5b2fb06df3c46126b35160e8ec3f3f4
Fabian Braun <fsbraun@gmx.de>
2022-12-08 15:41:23 +0100

    Add: Allow simple version management commands from the page tree indicator drop down menus (#295)
    * adj toolbar tests to reflect unified spelling
    * Fix:		Catch cms.pagetree.js` ajax posts
    * Add:		Indicator status draft
    * Add:		Patch edit pagecontent only for draft versions
    * Fix:		Only offer to copy plugins from languages that allow for that
    * Fix:		Edit button only if it is needed
    * Add:		Revert button for unpublished versions
    * Fix:		Use logic from Contitions() to not repeat oneself
    * Removed unused method add_revert_button
    * Fix:		Only offer view published if get_absolute_url returns non-empty string
    * Fix:		Sync indicator items and versioning admin actions according to `can_be_...` methods.
    * unify icons
    * fix import
    * fix: no double edit option

---
+++ djangocms_versioning/indicators.py
@@ -0,0 +1,137 @@
+from django.contrib.auth import get_permission_codename
+from django.urls import reverse
+from django.utils.http import urlencode
+from django.utils.translation import gettext_lazy as _
+
+from djangocms_versioning.constants import (
+    ARCHIVED,
+    DRAFT,
+    PUBLISHED,
+    UNPUBLISHED,
+    VERSION_STATES,
+)
+from djangocms_versioning.helpers import version_list_url
+from djangocms_versioning.models import Version
+
+
+class IndicatorStatusMixin:
+    # Step 1:  The legend
+    @property
+    def indicator_descriptions(self):
+        return {
+            "published": _("Published"),
+            "dirty": _("Changed"),
+            "draft": _("Draft"),
+            "unpublished": _("Unpublished"),
+            "archived": _("Archived"),
+            "empty": _("Empty"),
+        }
+
+    @classmethod
+    def get_indicator_menu(cls, request, page_content):
+        menu_template = "admin/cms/page/tree/indicator_menu.html"
+        status = page_content.content_indicator()
+        if not status or status == "empty":
+            return super().get_indicator_menu(request, page_content)
+        versions = page_content._version  # Cache from .content_indicator() (see mixin above)
+        user = request.user
+        menu = []
+        if user.has_perm(f"cms.{get_permission_codename('change', versions[0]._meta)}"):
+            if versions[0].check_publish.as_bool(user):
+                menu.append((
+                    _("Publish"), "cms-icon-publish",
+                    reverse("admin:djangocms_versioning_pagecontentversion_publish", args=(versions[0].pk,)),
+                    "js-cms-tree-lang-trigger",  # Triggers POST from the frontend
+                ))
+            if versions[0].check_edit_redirect.as_bool(user) and versions[0].state == PUBLISHED:
+                menu.append((
+                    _("Create new draft"), "cms-icon-edit-new",
+                    reverse("admin:djangocms_versioning_pagecontentversion_edit_redirect", args=(versions[0].pk,)),
+                    "js-cms-tree-lang-trigger js-cms-pagetree-page-view",  # Triggers POST from the frontend
+                ))
+            if versions[0].check_revert.as_bool(user) and versions[0].state == UNPUBLISHED:
+                # Do not offer revert from unpublish -> archived versions to be managed in version admin
+                label = _("Revert from Unpublish")
+                menu.append((
+                    label, "cms-icon-undo",
+                    reverse("admin:djangocms_versioning_pagecontentversion_revert", args=(versions[0].pk,)),
+                    "js-cms-tree-lang-trigger",  # Triggers POST from the frontend
+                ))
+            if versions[0].check_unpublish.as_bool(user):
+                menu.append((
+                    _("Unpublish"), "cms-icon-unpublish",
+                    reverse("admin:djangocms_versioning_pagecontentversion_unpublish", args=(versions[0].pk,)),
+                    "js-cms-tree-lang-trigger",
+                ))
+            if len(versions) > 1 and versions[1].check_unpublish.as_bool(user):
+                menu.append((
+                    _("Unpublish"), "cms-icon-unpublish",
+                    reverse("admin:djangocms_versioning_pagecontentversion_unpublish", args=(versions[1].pk,)),
+                    "js-cms-tree-lang-trigger",
+                ))
+            if versions[0].check_discard.as_bool(user):
+                menu.append((
+                    _("Delete Draft") if status == DRAFT else _("Delete Changes"), "cms-icon-bin",
+                    reverse("admin:djangocms_versioning_pagecontentversion_discard", args=(versions[0].pk,)),
+                    "",  # Let view ask for confirmation
+                ))
+            if len(versions) >= 2 and versions[0].state == DRAFT and versions[1].state == PUBLISHED:
+                menu.append((
+                    _("Compare Draft to Published..."), "cms-icon-layers",
+                    reverse("admin:djangocms_versioning_pagecontentversion_compare", args=(versions[1].pk,)) +
+                    "?" + urlencode(dict(
+                        compare_to=versions[0].pk,
+                        back=reverse("admin:cms_page_changelist"),
+                    )),
+                    "",
+                ))
+        menu.append(
+            (
+                _("Manage Versions..."), "cms-icon-copy",
+                version_list_url(versions[0].content),
+                "",
+            )
+        )
+        return menu_template if menu else "", menu
+
+
+def content_indicator(page_content):
+    """Translates available versions into status to be reflected by the indicator.
+    Function caches the result in the page_content object"""
+
+    if not hasattr(page_content, "_indicator_status"):
+        versions = Version.objects.filter_by_content_grouping_values(
+            page_content
+        ).order_by("-pk")
+        signature = {
+            state: versions.filter(state=state)
+            for state, name in VERSION_STATES
+        }
+        if signature[DRAFT] and not signature[PUBLISHED]:
+            page_content._indicator_status = "draft"
+            page_content._version = signature[DRAFT]
+        elif signature[DRAFT] and signature[PUBLISHED]:
+            page_content._indicator_status = "dirty"
+            page_content._version = (signature[DRAFT][0], signature[PUBLISHED][0])
+        elif signature[PUBLISHED]:
+            page_content._indicator_status = "published"
+            page_content._version = signature[PUBLISHED]
+        elif signature[UNPUBLISHED]:
+            page_content._indicator_status = "unpublished"
+            page_content._version = signature[UNPUBLISHED]
+        elif signature[ARCHIVED]:
+            page_content._indicator_status = "archived"
+            page_content._version = signature[ARCHIVED]
+        else:
+            page_content._indicator_status = None
+            page_content._version = [None]
+    return page_content._indicator_status
+
+
+# Step 4: Check if current version is editable
+def is_editable(page_content, request):
+    if not page_content.content_indicator():
+        # Something's wrong: content indicator not identified. Maybe no version?
+        return False
+    versions = page_content._version
+    return versions[0].check_modify.as_bool(request.user)
--- djangocms_versioning/monkeypatch/templatetags.py
+++ djangocms_versioning/monkeypatch/templatetags.py
@@ -2,18 +2,13 @@ from cms.templatetags import cms_admin
 from cms.utils.urlutils import admin_reverse
 
 
-@cms_admin.register.simple_tag(takes_context=False)
 def get_admin_url_for_language(page, language):
     # TODO Perhaps modify get_languages so that it returns
     # only published languages, or add a separate function
     # to do so in places like this.
     existing_language = language in page.get_languages()
     if existing_language:
-        try:
-            page_content = page.get_title_obj(language, fallback=False)
-        except AttributeError:
-            page_content = page.get_content_obj(language, fallback=False)
-
+        page_content = page.get_content_obj(language, fallback=False)
         existing_language = bool(page_content)
     if not existing_language:
         admin_url = admin_reverse("cms_pagecontent_add")
@@ -22,4 +17,12 @@ def get_admin_url_for_language(page, language):
     return admin_reverse("cms_pagecontent_change", args=[page_content.pk])
 
 
-cms_admin.get_admin_url_for_language = get_admin_url_for_language  # noqa: E305
+if hasattr(cms_admin, "GetAdminUrlForLanguage"):
+    # Patch only if classy tag is there (4.1....)
+    def _get_admin(self, context, page, language):
+        return get_admin_url_for_language(page, language)
+
+    cms_admin.GetAdminUrlForLanguage.get_admin_url_for_language = _get_admin
+else:
+    # Patch only if classy tag is not there (4.0....).
+    cms_admin.get_admin_url_for_language = get_admin_url_for_language  # noqa: E305

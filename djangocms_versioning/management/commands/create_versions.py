090122aeeb57b5c599c33d0b0249e1f1ef8e3d11
Fabian Braun <fsbraun@gmx.de>
2023-01-10 15:25:00 +0100

    feat: create version objects if adding versioning to existing models

--- djangocms_versioning/management/commands/create_versions.py
+++ djangocms_versioning/management/commands/create_versions.py
@@ -1,28 +1,96 @@
+from django.contrib.auth import get_user_model
+from django.contrib.contenttypes.models import ContentType
 from django.core.management.base import BaseCommand, CommandError
 
 from djangocms_versioning import constants
+from djangocms_versioning.conf import DEFAULT_USER, USERNAME_FIELD
 from djangocms_versioning.models import Version
 from djangocms_versioning.versionables import _cms_extension
 
 
-# from polls.models import Question as Poll
+User = get_user_model()
+
 
 class Command(BaseCommand):
     help = 'Creates Version objects for versioned models lacking one'
 
     def add_arguments(self, parser):
-        parser.add_argument("state", type=str, default=constants.DRAFT)
+        parser.add_argument(
+            "--state",
+            type=str,
+            default=constants.DRAFT,
+            choices=[key for key, value in constants.VERSION_STATES],
+            help="state of newly created version object"
+        )
+        parser.add_argument(
+            "--username",
+            type=str,
+            nargs="?",
+            help="Username of user to create the missing Version objects"
+        )
+        parser.add_argument(
+            "--userid",
+            type=int,
+            nargs="?",
+            help="User id of user to create the missing Version objects"
+        )
+
+        parser.add_argument(
+            "--dry-run",
+            action="store_true",
+            help="Do not change the database",
+        )
 
     def get_user(self, options):
-        pass
+        user = None
+        if options["userid"] and options["username"]:
+            raise CommandError("Only either one of the options '--userid' or '--username' may be given")
+        if options["userid"]:
+            try:
+                user = User.objects.get(pk=options["userid"])
+            except User.DoesNotExist:
+                raise CommandError(f"No user with id {options['userid']} found")
+        if options["username"]:
+            try:
+                user = User.objects.get(**{USERNAME_FIELD: options["username"]})
+            except User.DoesNotExist:
+                raise CommandError(f"No user with name {options['username']} found")
+        if user is None and DEFAULT_USER is not None:
+            try:
+                user = User.objects.get(pk=DEFAULT_USER)
+            except User.DoesNotExist:
+                raise CommandError(f"No user with id {DEFAULT_USER} found "
+                                   f"(specified as DJANGOCMS_VERSIONING_DEFAULT USER in settings.py")
+        return user
 
     def handle(self, *args, **options):
-        if options["state"] not in dict(constants.VERSION_STATES):
-            raise CommandError(f"state needs to be one of {', '.join(key for key, value in constants.VERSION_STATES)}")
         user = self.get_user(options)
-        versionables = _cms_extension().versionables
-        print(f"{versionables=}")
-        for model in versionables:
-            pass
 
-        self.stdout.write(self.style.SUCCESS('Successfully closed poll "%s"' % 1))
+        for versionable in _cms_extension().versionables:
+            Model = versionable.content_model
+            content_type = ContentType.objects.get_for_model(Model)
+            version_ids = Version.objects.filter(content_type_id=content_type.pk).values_list("object_id", flat=True)
+            unversioned = Model.admin_manager.exclude(pk__in=version_ids).order_by("-pk")
+            self.stdout.write(self.style.NOTICE(
+                f"{len(version_ids) + len(unversioned)} objects of type {Model.__name__}, thereof "
+                f"{len(unversioned)} missing Version object"
+            ))
+            if not options["dry_run"]:
+                if user is None:
+                    raise CommandError("Please specify a user which missing Version objects shall belong to "
+                                       "either with the DJANGOCMS_VERSIONING_DEFAULT_USER setting or using "
+                                       "command line arguments")
+                for orphan in unversioned:
+                    try:
+                        Version.objects.create(
+                            content=orphan,
+                            state=options["state"],
+                            created_by=user,
+                        )
+                        self.stdout.write(self.style.SUCCESS(
+                            f"Successfully created version object for {Model.__name__} with pk={orphan.pk}"
+                        ))
+                    except Exception as e:
+                        self.stdout.write(self.style.ERROR(
+                            f"Failed creating version object for {Model.__name__} with pk={orphan.pk}: {e}"
+                        ))

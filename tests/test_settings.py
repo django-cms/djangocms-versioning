155fb7925c643aefaa355e8dd24e29da12d01630
Fabian Braun <fsbraun@gmx.de>
2023-02-24 14:53:32 +0100

    Update test

--- tests/test_settings.py
+++ tests/test_settings.py
@@ -1,16 +1,19 @@
+from django.conf import settings
 from django.db import models
 from django.test import override_settings
 
 from cms.test_utils.testcases import CMSTestCase
 
 from djangocms_versioning import constants
-from djangocms_versioning.models import Version
+from djangocms_versioning import models as versioning_models
 from djangocms_versioning.test_utils import factories
 
 
 class DeletionTestCase(CMSTestCase):
     @override_settings(DJANGOCMS_VERSIONING_ALLOW_DELETING_VERSIONS=False)
     def test_deletion_not_possible(self):
+        # Since djangocms_versionings.models stores the setting, we need to update that copy
+        versioning_models.ALLOW_DELETING_VERSIONS = settings.DJANGOCMS_VERSIONING_ALLOW_DELETING_VERSIONS
         poll = factories.PollFactory()
         version1 = factories.PollVersionFactory(
             content__poll=poll,
@@ -19,7 +22,7 @@ class DeletionTestCase(CMSTestCase):
         pk1 = version1.pk
         # Now publish and then edit redirect to create a draft on top of published version
         version1.publish(user=self.get_superuser())
-        self.assertEqual(Version.objects.get(pk=pk1).state, constants.PUBLISHED)
+        self.assertEqual(versioning_models.Version.objects.get(pk=pk1).state, constants.PUBLISHED)
 
         version2 = version1.copy(created_by=self.get_superuser())
         version2.save()
@@ -29,10 +32,12 @@ class DeletionTestCase(CMSTestCase):
 
         # try deleting and see if error is raised
         self.assertRaises(models.deletion.ProtectedError,
-                          Version.objects.get(pk=pk1).content.delete)
+                          versioning_models.Version.objects.get(pk=pk1).content.delete)
 
     @override_settings(DJANGOCMS_VERSIONING_ALLOW_DELETING_VERSIONS=True)
     def test_deletion_possible(self):
+        # Since djangocms_versionings.models stores the setting, we need to update that copy
+        versioning_models.ALLOW_DELETING_VERSIONS = settings.DJANGOCMS_VERSIONING_ALLOW_DELETING_VERSIONS
         poll = factories.PollFactory()
         version1 = factories.PollVersionFactory(
             content__poll=poll,
@@ -41,7 +46,7 @@ class DeletionTestCase(CMSTestCase):
         pk1 = version1.pk
         # Now publish and then edit redirect to create a draft on top of published version
         version1.publish(user=self.get_superuser())
-        self.assertEqual(Version.objects.get(pk=pk1).state, constants.PUBLISHED)
+        self.assertEqual(versioning_models.Version.objects.get(pk=pk1).state, constants.PUBLISHED)
 
         version2 = version1.copy(created_by=self.get_superuser())
         version2.save()
@@ -50,5 +55,5 @@ class DeletionTestCase(CMSTestCase):
         self.assertIsNotNone(version2.source)
 
         # try deleting and see if error is raised
-        Version.objects.get(pk=pk1).content.delete()
-        self.assertIsNone(Version.objects.get(pk=version2.pk).source)
+        versioning_models.Version.objects.get(pk=pk1).content.delete()
+        self.assertIsNone(versioning_models.Version.objects.get(pk=version2.pk).source)

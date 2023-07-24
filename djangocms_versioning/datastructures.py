7651a5d7bcd022cb973942c0621567c2b1159bd7
Fabian Braun <fsbraun@gmx.de>
2023-07-24 11:47:18 +0200

    feat: Let default_copy also copy PlaceholderRelationFields

--- djangocms_versioning/datastructures.py
+++ djangocms_versioning/datastructures.py
@@ -1,5 +1,6 @@
 from itertools import chain
 
+from cms.models import Placeholder, PlaceholderRelationField
 from django.contrib.contenttypes.models import ContentType
 from django.db.models import Max, Prefetch
 from django.utils.functional import cached_property
@@ -219,5 +220,29 @@ def default_copy(original_content):
         # don't copy primary key because we're creating a new obj
         if content_model._meta.pk.name != field.name
     }
-    # Use original manager to avoid creating a new draft version here!
-    return content_model._original_manager.create(**content_fields)
+    # Use original manager to not create a new Version object here
+    new_content = content_model._original_manager.create(**content_fields)
+
+    # Now copy PlaceholderRelationFields
+    for field in content_model._meta.private_fields:
+        # Copy PlaceholderRelationFields
+        if isinstance(field, PlaceholderRelationField):
+            # Copy placeholders
+            new_placeholders = []
+            for placeholder in getattr(original_content, field.name).all():
+                placeholder_fields = {
+                    field.name: getattr(placeholder, field.name)
+                    for field in Placeholder._meta.fields
+                    # don't copy primary key because we're creating a new obj
+                    # and handle the source field later
+                    if field.name not in [Placeholder._meta.pk.name, "source"]
+                }
+                if placeholder.source:
+                    placeholder_fields["source"] = new_content
+                new_placeholder = Placeholder.objects.create(**placeholder_fields)
+                # Copy plugins
+                placeholder.copy_plugins(new_placeholder)
+                new_placeholders.append(new_placeholder)
+            getattr(new_content, field.name).add(*new_placeholders)
+
+    return new_content

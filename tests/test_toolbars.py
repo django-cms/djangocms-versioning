2c5e1e2de00920a37dcf42b7accc00471eed02b3
Fabian Braun <fsbraun@gmx.de>
2025-03-14 12:49:39 +0100

    Only show language menu for more than one language

--- tests/test_toolbars.py
+++ tests/test_toolbars.py
@@ -6,6 +6,7 @@ from cms import __version__
 from cms.cms_toolbars import LANGUAGE_MENU_IDENTIFIER, PlaceholderToolbar
 from cms.test_utils.testcases import CMSTestCase
 from cms.toolbar.utils import get_object_edit_url, get_object_preview_url
+from cms.utils.i18n import get_language_tuple
 from cms.utils.urlutils import admin_reverse
 from django.contrib.auth.models import Permission
 from django.test import override_settings
@@ -494,6 +495,20 @@ class VersioningPageToolbarTestCase(CMSTestCase):
             if hasattr(item, "name") and item.name == name:
                 return item
         return None
+
+    @override_settings(CMS_LANGUAGES = {1: [{'code': 'en', 'name': 'English'}]})
+    def test_change_language_menu_page_toolbar_one_languages(self):
+        page_content = PageContentWithVersionFactory()
+        request = self.get_page_request(
+            page=page_content.page,
+            path=get_object_edit_url(page_content),
+            user=self.get_superuser(),
+        )
+        request.toolbar.set_object(page_content)
+        request.toolbar.populate()
+        request.toolbar.post_template_populate()
+        language_menu = request.toolbar.get_menu(LANGUAGE_MENU_IDENTIFIER)
+        self.assertIsNone(language_menu)
 
     def test_change_language_menu_page_toolbar(self):
         """Check that patched PageToolbar.change_language_menu only provides

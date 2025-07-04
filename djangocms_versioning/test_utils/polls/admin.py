5f8fa13861257bac383a7f2f672336c96c7dd268
Fabian Braun <fsbraun@gmx.de>
2025-07-04 13:03:51 +0200

    Fix ruff

--- djangocms_versioning/test_utils/polls/admin.py
+++ djangocms_versioning/test_utils/polls/admin.py
@@ -3,7 +3,6 @@ from django.contrib import admin
 from django.urls import re_path
 
 from djangocms_versioning.admin import (
-    ExtendedGrouperVersionAdminMixin,
     ExtendedVersionAdminMixin,
 )
 

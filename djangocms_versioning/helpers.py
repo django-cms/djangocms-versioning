763cb5cd754a5abfc938731143cfe477bb29d851
Vinit kumar <vinit@django-cms.org>
2025-10-25 03:44:59 +0530

    Fix type hint for replace_admin_for_models pairs parameter
    - Change from 'tuple[type[models.Model], type]' to 'Iterable[tuple[type[models.Model], type]]'
    - The function expects multiple pairs, not a single tuple
    - Also fixed docstring parameter name from 'models' to 'pairs'

--- djangocms_versioning/helpers.py
+++ djangocms_versioning/helpers.py
@@ -70,9 +70,9 @@ def _replace_admin_for_model(modeladmin: type[admin.ModelAdmin], mixin: type, ad
     admin_site.register(modeladmin.model, new_admin_class)
 
 
-def replace_admin_for_models(pairs: tuple[type[models.Model], type], admin_site: admin.AdminSite | None = None):
+def replace_admin_for_models(pairs: Iterable[tuple[type[models.Model], type]], admin_site: admin.AdminSite | None = None):
     """
-    :param models: List of (model class, admin mixin class) tuples
+    :param pairs: Iterable of (model class, admin mixin class) tuples
     :param admin_site: AdminSite instance
     """
     if admin_site is None:
